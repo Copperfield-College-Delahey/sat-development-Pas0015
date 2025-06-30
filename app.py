86498a0ff7cfbc67a0eeebc8de9665117b47e501
Pas0015 <pas0015@copperfieldcollege.vic.edu.au>
2025-06-30 13:31:01 +1000

    started code

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 #Study Planner App
+import customtkinter as ctk
+from CTkTable import *
+from paes.searchPage import searchPage
+from pages.addPage import addPage 
+
+
+
+app = ctk.CTk()
+app.Title("Study Planner")
+app.geometry("1000x600")
+# Configure app window grid
+app.grid_column
+
+
+
+
+
+
+
+
+app.mainloop
