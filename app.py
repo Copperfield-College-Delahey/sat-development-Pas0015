ee198d429f0f63e69c0021cab0fa83e6c82fb897
Pas0015 <pas0015@copperfieldcollege.vic.edu.au>
2025-07-01 11:34:04 +1000

    layout

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #Study Planner App
+from tkinter import *
 import customtkinter as ctk
 from CTkTable import *
 from paes.searchPage import searchPage
@@ -10,10 +11,14 @@ app = ctk.CTk()
 app.Title("Study Planner")
 app.geometry("1000x600")
 # Configure app window grid
-app.grid_column
+left_frame  =  Frame(app,  width=200,  height=  400,  bg='black')
+left_frame.grid(row=0,  column=0,  padx=10,  pady=5)
+
+right_frame  =  Frame(app,  width=650,  height=400,  bg='white')
+right_frame.grid(row=0,  column=1,  padx=10,  pady=5)
 
 
-
+    
 
 
 
